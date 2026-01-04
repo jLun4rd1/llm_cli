86bc9b78beba2cfd11cada3f3d21dcf2c393dcfe
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-04 12:57:41 -0300

    :sparkles: feat - add --verbose argument that shows token metadata and create 'messages', a variable that holds a list of Contents in order to keep track of history

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,21 +16,25 @@ def get_client():
 def get_args_from_parser():
     parser = argparse.ArgumentParser(description="LLM CLI")
     parser.add_argument("user_prompt", type=str, help="Your input. Use it with wisdom")
+    parser.add_argument("--verbose", action="store_true", help="Enables verbose output, such as tokens metadata")
     return parser.parse_args()
 
 def main():
     client = get_client()
     args = get_args_from_parser()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model=model,
-        contents=args.user_prompt
+        contents=messages
     )
     if not response.usage_metadata:
         raise RuntimeError("No 'usage_metadata' property in response. Can't access token count")
 
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 
