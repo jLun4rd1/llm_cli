171721de9e1ea9dcd9768476af1345d5c691d04a
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-05 15:57:29 -0300

    :sparkles: feat - first baby step. Teach llm what it can do by writing a system prompt and adding it to its config. Also change the response printed so that the program prints the function call if that's what the llm decided to do or its text response otherwise.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -26,7 +28,12 @@ def main():
 
     response = client.models.generate_content(
         model=model,
-        contents=messages
+        contents=messages,
+        # If disobeying happens, set the 'temperature' argument of GenerateContentConfig to 0
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        ),
     )
     if not response.usage_metadata:
         raise RuntimeError("No 'usage_metadata' property in response. Can't access token count")
@@ -35,7 +42,12 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
