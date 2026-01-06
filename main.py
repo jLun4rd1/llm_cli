0950fc66c3fa5d10e786a032e0ea9c672d24d3ed
jLun4rd1 <joaoclunardi@gmail.com>
2026-01-05 23:09:28 -0300

    :sparkles: feat - implement loop and append function responses to the messages list so that AI is aware of the history

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
@@ -26,41 +27,53 @@ def main():
     args = get_args_from_parser()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(
-        model=model,
-        contents=messages,
-        # If disobeying happens, set the 'temperature' argument of GenerateContentConfig to 0
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
-        ),
-    )
-    if not response.usage_metadata:
-        raise RuntimeError("No 'usage_metadata' property in response. Can't access token count")
+    for _ in range(10):
+        response = client.models.generate_content(
+            model=model,
+            contents=messages,
+            # If disobeying happens, set the 'temperature' argument of GenerateContentConfig to 0
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+            ),
+        )
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if not response.usage_metadata:
+            raise RuntimeError("No 'usage_metadata' property in response. Can't access token count")
 
-    function_results = []
-    if response.function_calls:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-            if not function_call_result.parts:
-                raise Exception("Missing 'parts' list in types.Content object")
-            if not function_call_result.parts[0].function_response:
-                raise Exception("Missing 'function_response' property")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("Missing called function response")
+        function_results = []
+        if response.function_calls:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, args.verbose)
 
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-            function_results.append(function_call_result.parts[0])
+                if not function_call_result.parts:
+                    raise Exception("Missing 'parts' list in types.Content object")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("Missing 'function_response' property")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("Missing called function response")
 
-    else:
-        print(f"Response:\n{response.text}")
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                function_results.append(function_call_result.parts[0])
+
+        else:
+            print(f"Response:\n{response.text}")
+            break
+
+        messages.append(types.Content(role="user", parts=function_results))
+
+        if _ == 9:
+           print("Maximum number of iterations reached")
+           sys.exit(1)
 
 
 if __name__ == "__main__":
